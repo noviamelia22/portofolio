00f9667a94f2cd5f10929e62e973e54eef290ae5
noviamelia22 <ameliakristantinovi@gmail.com>
2025-05-08 03:23:31 +0700

    Trigger Vercel redeploy

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -51,29 +51,25 @@ const Contact = () => {
                         </div>
                         <div className="contact-detail">
                             <i className="fab fa-github"></i>
-                            <p>
-                                <a
-                                    href="https://github.com/noviamelia22"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    className="contact-link"
-                                >
-                                    noviamelia22
-                                </a>
-                            </p>
+                            <a
+                                href="https://github.com/noviamelia22"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="contact-link"
+                            >
+                                noviamelia22
+                            </a>
                         </div>
                         <div className="contact-detail">
                             <i className="fab fa-linkedin"></i>
-                            <p>
-                                <a
-                                    href="https://linkedin.com/in/novitanti22"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    className="contact-link"
-                                >
-                                    novitanti22
-                                </a>
-                            </p>
+                            <a
+                                href="https://linkedin.com/in/novitanti22"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="contact-link"
+                            >
+                                novitanti22
+                            </a>
                         </div>
                     </div>
                 </div>
